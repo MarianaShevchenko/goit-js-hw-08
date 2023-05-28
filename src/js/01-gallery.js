b95d9cd9aac26aa69c6f366887ddeb8c52a48ca2
MarianaShevchenko <shevchenkomariana3@gmail.com>
2023-05-28 17:53:44 +0300

    task-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markupArray = galleryItems.map(function (item) {
+  const itemMarkup = `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+
+  return itemMarkup;
+});
+
+galleryRef.innerHTML = markupArray.join('');
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// const listItems = document.querySelectorAll('.gallery li');
+// listItems.forEach(function (item) {
+//   item.style.listStyleType = 'none';
+// });
